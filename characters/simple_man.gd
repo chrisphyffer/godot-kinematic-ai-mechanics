0fbfe97b19963f24fa79c7ee5491fde3a7f1d397
Chris Phyffer <chris.supernerd@gmail.com>
2019-08-09 11:40:13 -0700

    Typecasting, separating scripts. Inheritance: Player, PlayableCharacter, AI, Pawns
    Signed-off-by: Chris Phyffer <chris.supernerd@gmail.com>

--- characters/simple_man.gd
+++ characters/simple_man.gd
@@ -6,14 +6,6 @@ extends KinematicBody
 
 class_name Character
 
-
-func set_paths(paths_arg):
-	accumulated_rotation = 0
-	paths = paths_arg
-	arrived_at_position = false
-	rotated_to_position = false
-	$AnimationTree["parameters/Locomotion/blend_position"] = 1
-
 var paths = []
 var accumulated_rotation = 0
 var arrived_at_position = false
@@ -21,7 +13,6 @@ var rotated_to_position = false
 var orientation = Transform()
 var velocity = Vector3()
 const GRAVITY = Vector3(0,-9.8, 0)
-var next_path = false
 
 export var is_ai = false
 
@@ -31,24 +22,50 @@ export(Array, NodePath) var waypoints
 
 var navLevel
 
+export(bool) var draw_path = true
+export(bool) var teleport_on_fail = false
+
+const PATH_FAIL_MAX = 3
+var path_fail = 0
+const MAX_ERRORS = 10
+var errors = 0
+
+const INTERVAL_TO_CHECK_DISTANCE_MOVED = 1
+var time_distance_moved = 0
+var beginning_position = Vector3()
+var CHARACTER_STUCK_DEADZONE = .2
+
+
+# If I cannot access my destination time (whether there is someone in the way or
+# I simply cannot get there...then teleport me there.
+const DESTINATION_ACCESS_TIME = 6000
+var remaining_access_time = DESTINATION_ACCESS_TIME
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	orientation=global_transform
 	orientation.origin = Vector3()
 	navLevel = get_tree().get_root().find_node('Level', true, false)
 
-func set_body_material(mat):
+	if is_ai:
+		$AnimationTree["parameters/Locomotion/blend_position"] = 2
+		#$AnimationTree["parameters/Locomotion/blend_position"] = 1
+
+func set_body_material(mat: Material):
 	find_node('SimpleManMesh').set_surface_material(0, mat)
 
+
 func _process_movement(delta):
+
+	if typeof(paths) != TYPE_ARRAY:
+		$AnimationTree["parameters/Locomotion/blend_position"] = 0
+		return
+
 	if paths.empty():
 		$AnimationTree["parameters/Locomotion/blend_position"] = 0
 		return
 
 	if global_transform.origin.distance_to(paths[0]) < .5:
-		#print(arrived_at_position)
-		#arrived_at_position = true
-		#rotated_to_position = false
 		accumulated_rotation = 0
 		paths.remove(0)
 
@@ -61,7 +78,6 @@ func _process_movement(delta):
 	var rotTransform = t.looking_at(paths[0], Vector3(0,1,0))
 	var thisRotation = Quat(t.basis).slerp( rotTransform.basis, clamp(accumulated_rotation, 0, 1) )
 
-
 	accumulated_rotation += delta
 
 	if accumulated_rotation > 1:
@@ -70,7 +86,8 @@ func _process_movement(delta):
 	if not rotated_to_position:
 		orientation.basis = Transform(thisRotation, t.origin).basis
 
-	$AnimationTree["parameters/Locomotion/blend_position"] = 1
+	$AnimationTree["parameters/Locomotion/blend_position"] = 2
+	$AnimationTree["parameters/LocomotionTimeScale/scale"] = 1
 
 	var root_motion = $AnimationTree.get_root_motion_transform()
 
@@ -90,17 +107,118 @@ func _process_movement(delta):
 
 	pass
 
-var time_test = 0
-const MAX_TIME_TEST = 2
+
+
 func _physics_process(delta):
-	_process_movement(delta)
+
+	if is_ai and path_fail:
+		return
 
 	if is_ai:
-		var waypoint = get_node( waypoints[ int( round( rand_range( 0, waypoints.size() - 1 ) ) ) ] )
-	
-		if paths.empty(): #and time_test > MAX_TIME_TEST:
-			paths = navLevel.generate_path(self, waypoint)
-			#time_test = 0
-			print('Update Nav Level')
-			
-		#time_test += delta
+		if paths.empty():
+			if not generate_paths(grab_random_waypoint()):
+				return
+				# Try again, otherwise, just idle this pawn.
+
+			remaining_access_time = DESTINATION_ACCESS_TIME
+
+		# Is our character unable to reach their destination in time?
+		if remaining_access_time <= 0:
+			remaining_access_time = DESTINATION_ACCESS_TIME
+
+			if teleport_on_fail:
+				global_transform.origin = paths[paths.size()-1]
+				paths = []
+			else:
+				if not generate_paths(grab_random_waypoint()):
+					return
+
+			print('Could Not reach destination in time.. Teleporting: ', teleport_on_fail)
+
+		remaining_access_time -= delta
+
+		# Is our character stuck?
+		time_distance_moved += delta
+
+		if time_distance_moved >= INTERVAL_TO_CHECK_DISTANCE_MOVED: #If one second has elapsed
+			var total_position = transform.origin - beginning_position # Generate a Vector difference between the two.
+			if total_position.length() < CHARACTER_STUCK_DEADZONE: # The character might be stuck.
+
+				# Find nearest waypoint behind the character.
+				find_nearest_waypoint_behind = true
+
+				#generate_paths(grab_random_waypoint())
+
+			beginning_position = transform.origin # Record this beginning position
+			time_distance_moved = 0
+
+	_process_movement(delta)
+	_check_vision(delta)
+
+func grab_random_waypoint():
+	var waypoint
+	while not waypoint:
+		waypoint = get_tree().get_root().find_node( waypoints[ int( round( rand_range( 0, waypoints.size() - 1 ) ) ) ], true, false )
+		errors += 1
+
+	return waypoint
+
+func generate_paths(waypoint: Node):
+	paths = navLevel.generate_path(self, waypoint.get_translation())
+	if paths and not paths.empty():
+		return true
+
+	path_fail = true
+	print('Path Fail')
+	return false
+
+# Player Only
+func set_path(paths_arg: Array):
+	accumulated_rotation = 0
+	paths = paths_arg
+	arrived_at_position = false
+	rotated_to_position = false
+	$AnimationTree["parameters/Locomotion/blend_position"] = 1
+
+
+var find_nearest_waypoint_behind = false
+
+##################
+# CHECK VISION
+func _check_vision(delta):
+	#Scan for waypoints behind this character.
+	if find_nearest_waypoint_behind:
+		#print('Character stuck, finding nearest waypoint away from my facing direction')
+		$AreaOfAwareness.monitoring = true
+		for i in range(0, bodies_in_awareness.size()-1):
+			if bodies_in_awareness[i].get_owner().is_in_group('Waypoint'):
+				var target_waypoint = bodies_in_awareness[i]
+				navLevel.generate_path(self, target_waypoint)
+				find_nearest_waypoint_behind = false
+				$AreaOfAwareness.monitoring = false
+				#print('YAS')
+
+				#var AP = (target_waypoint.get_transform().origin - transform.origin).normalized()
+				#if AP.dot(transform.basis.x) >= 0:
+				#	print('SEEING WAYPOINT.')
+				#	navLevel.generate_path(self, target_waypoint)
+				#	find_nearest_waypoint_behind = false
+				#	break
+		
+		if find_nearest_waypoint_behind:
+			generate_paths(grab_random_waypoint())
+			find_nearest_waypoint_behind = false
+			$AreaOfAwareness.monitoring = false
+
+	pass
+
+var bodies_in_awareness = []
+func _object_entered_area_of_awareness(body):
+	bodies_in_awareness.append(body)
+	pass
+
+func _object_exited_area_of_awareness(body):
+	for i in range(0, bodies_in_awareness.size()-1):
+		if bodies_in_awareness[i] == body:
+			bodies_in_awareness.remove(i)
+			break
--- nav_mesh_root_driven.gd
+++ nav_mesh_root_driven.gd
@@ -4,127 +4,124 @@ extends Navigation
 # Member variables
 const SPEED = 4.0
 
-var camrot = 0.0
+#var begin = Vector3()
+#var end = Vector3()
 
-var begin = Vector3()
-var end = Vector3()
-var m = SpatialMaterial.new()
-
-var path = []
-var draw_path = true
+#var path = []
+#var draw_path = true
 
 const ROTATION_SPEED = 1
 
-func _process(delta):
+var player = null
 
-	return
-
-	if (path.size() > 1):
-		var to_walk = delta*SPEED
-		var to_watch = Vector3(0, 1, 0)
-		while(to_walk > 0 and path.size() >= 2):
-			var pfrom = path[path.size() - 1]
-			var pto = path[path.size() - 2]
-			to_watch = (pto - pfrom).normalized()
-			var d = pfrom.distance_to(pto)
-			if (d <= to_walk):
-				path.remove(path.size() - 1)
-				to_walk -= d
-			else:
-				path[path.size() - 1] = pfrom.linear_interpolate(pto, to_walk/d)
-				to_walk = 0
-
-		var atpos = path[path.size() - 1]
-		var atdir = to_watch
-		atdir.y = 0
-
-		var t = Transform()
-		t.origin = atpos
-		t=t.looking_at(atpos + atdir, Vector3(0, 1, 0))
-		get_node("Robot").set_transform(t)
-
-		if (path.size() < 2):
-			path = []
-			set_process(false)
-	else:
-		set_process(false)
-
-
-func _update_path():
-	var p = get_simple_path(begin, end, true)
-	path = Array(p) # Vector3array too complex to use, convert to regular array
-	#path.invert()
-
-	if path.size() and not path.empty():
-		for i in range(0, path.size() ):
-			#path[i].y = 0
-			pass
-
-		$SimpleMan.set_paths(path)
-	#set_process(true)
-
-	if (draw_path):
-		var im = get_node("Draw")
-		im.set_material_override(m)
-		im.clear()
-		im.begin(Mesh.PRIMITIVE_POINTS, null)
-		
-		im.add_vertex(Vector3(begin.x, begin.y, begin.z ) )
-		im.add_vertex(Vector3(end.x, end.y, end.z ) )
-		im.end()
-		im.begin(Mesh.PRIMITIVE_LINE_STRIP, null)
-		for x in p:
-			im.add_vertex(x)
-		im.end()
-
+func _ready():
+	
+	player = get_tree().get_root().find_node('Player', true, false)
+	if not player:
+		get_tree().quit()
 
 func _input(event):
 #	if (event extends InputEventMouseButton and event.button_index == BUTTON_LEFT and event.pressed):
 	if (event.is_class("InputEventMouseButton") and event.button_index == BUTTON_LEFT and event.pressed):
 		var from = get_node("Camera").project_ray_origin(event.position)
 		var to = from + get_node("Camera").project_ray_normal(event.position)*100
-		var p = get_closest_point_to_segment(from, to)
-
-		begin = get_closest_point(get_node("SimpleMan").get_translation())
-		end = p
-
-		_update_path()
+		var end = get_closest_point_to_segment(from, to)
+		
+		var paths = generate_path(player, end)
+		if typeof(paths) == TYPE_ARRAY:
+			player.set_path(paths)
 
 var character_queues = []
 
-func generate_path(requester, destination):
-	print( requester.get_instance_id() )
-	
-	var start_path = get_closest_point(requester.get_translation())
-	var end_path = get_closest_point(destination.get_translation())
-	var p = get_simple_path(start_path, end_path, true)
-	var found_queue
+func update_character_queue(requester, start_path: Vector3, end_path: Vector3, paths: Array = [], draw_node: bool = false):
 
+	var found_queue = null
+
+	var dictionary_form = {
+		'name' : requester.get_instance_id(),
+		'begin' : start_path,
+		'end' : end_path,
+		'paths' : paths,
+		'draw_node' : draw_node
+	}
 	
 	for i in range(0, character_queues.size()):
 		if character_queues[i].name == requester.get_instance_id():
 			found_queue = i
 			break
-			
-	var dictionary_form = {
-		'name' : requester.get_instance_id(),
-		'begin' : start_path,
-		'end' : end_path,
-		'paths' : Array(p)
-	}
-		
+	
 	if found_queue:
+		dictionary_form.draw_node = character_queues[found_queue].draw_node
 		character_queues[found_queue] = dictionary_form
 	else:
 		character_queues.append(dictionary_form)
+		found_queue = character_queues.size()-1
+		
+	return found_queue
 	
-	return Array(p)
+
+func generate_path(requester, destination : Vector3):
 	
-	pass
+	var start_path = get_closest_point(requester.get_translation())
+	var end_path = get_closest_point(destination)
+	var p = get_simple_path(start_path, end_path, true)
+	var paths = Array(p)
+	
+	if not paths:
+		return
+	
+	var characterIndex = update_character_queue(requester, start_path, end_path, paths)
+	
+	if requester.draw_path:
+		character_queues[characterIndex].draw_node = do_draw_path(characterIndex)
+	
+	if Array(p):
+		return Array(p)
+	else:
+		return false
+	
+func path_completed(requester):
+	var found_queue
+	for i in range(0, character_queues.size()):
+		if character_queues[i].name == requester.get_instance_id():
+			found_queue = i
+			break
 
-func _ready():
-	set_process_input(true)
+	if found_queue != null:
+		var drawNode = character_queues[found_queue]['draw_node']
+		if drawNode:
+			drawNode.queue_free()
 
+func do_draw_path(characterIndex, player = false):
+	
+	var m = SpatialMaterial.new()
+	m.albedo_color = Color(255, 255, 255, .5)
 	m.flags_unshaded = true
 	m.flags_use_point_size = true
-	m.albedo_color = Color(1.0, 1.0, 1.0, 1.0)
+	#m.albedo_color = Color(1.0, 1.0, 1.0, 1.0)
+	
+	var drawNode = false
+	
+	if not character_queues[characterIndex].draw_node:
+		drawNode = ImmediateGeometry.new()
+		add_child(drawNode)
+	else:
+		drawNode = character_queues[characterIndex].draw_node
+		#print('drawing new node for character: ', character_queues[characterIndex].name)
+	
+	var paths = character_queues[characterIndex].paths
+		
+	
+	drawNode.set_material_override(m)
+	drawNode.clear()
+	drawNode.begin(Mesh.PRIMITIVE_POINTS, null)
+	
+	drawNode.add_vertex(Vector3(paths[0].x, paths[0].y, paths[0].z ) )
+	drawNode.add_vertex(Vector3(paths[paths.size()-1].x, paths[paths.size()-1].y, paths[paths.size()-1].z ) )
+	drawNode.end()
+	drawNode.begin(Mesh.PRIMITIVE_LINE_STRIP, null)
+	for x in paths:
+		drawNode.add_vertex(x)
+	drawNode.end()
+
+	return drawNode
